d9d52417a10cca855eebc9b7ec3c1cdaae0ba4a3
Rafał Klinowski <klinowskirafal@gmail.com>
2019-10-14 16:14:56 +0200

    Zaimplementowano program wyszukujący wartość najmniejszą i największą w tablicy jednowymiarowej.

---
+++ TablicaMinMax/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TablicaMinMax
+{
+	internal class Program
+	{
+		private static void ZnajdzMinMax(int[] tab)
+		{
+			var min = tab[0];
+			var max = tab[0];
+
+			for (int i = 1; i < tab.Length; i++)
+			{
+				if (tab[i] < min)
+				{
+					min = tab[i];
+				}
+				else if (tab[i] > max)
+				{
+					max = tab[i];
+				}
+			}
+
+			Console.WriteLine($"Najmniejsza liczba w tej tablicy jest równa: {min}");
+			Console.WriteLine($"Największa liczba w tej tablicy jest równa: {max}");
+		}
+
+		private static void Main()
+		{
+			var tab = new int[] { 1, 4, -5, 12, 123, 0, -44, 0 };
+			ZnajdzMinMax(tab);
+		}
+	}
+}
